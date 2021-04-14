4b5e4c38ce2197e71ae3ce4a547a5dae971e9577
TCH27 <edisontchondo@gmail.com>
2021-04-14 17:30:57 +0200

    changeme de l'id

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,7 +101,7 @@ function sendmail() {
     Email.send({
         SecureToken: "fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
         To: 'edisontchondo@gmail.com',
-        From: "you@isp.com",
+        From: "edisontchondo@gmail.com",
         Subject: "Nouveau message depuis le siteweb de la Saveur",
         Body: Body
     }).then(
